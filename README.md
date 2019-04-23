48b0c7aa210a60538c5076e51adb53e125d11fb1
Adam Hathcock <adam.hathcock@skybettingandgaming.com>
2019-04-23 15:36:30 +0100

    Initial conversion to passport-next

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # passport-oauth2-client-public
+
+This is a Strategy that allows only a `client_id` in the body of the request for verification.  This is intended just for the [OAuth2 PKCE Extension](https://github.com/passport-next/oauth2orize-pkce) protecting the `Token` endpoint.
--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,14 @@
 /* global describe, it */
 
-var pkg = require('..');
+var strategy = require('..');
 var expect = require('chai').expect;
 
 
 describe('passport-oauth2-client-public', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
-  });
+  it('should export Strategy constructor directly from package', function() {
+    expect(strategy).to.be.a('function');
+    expect(strategy).to.equal(strategy.Strategy);
+});
   
 });
